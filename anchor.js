d07de57908c63d925c2b4f42c5c71dc8b637ffe1
quack <87156593+jlhklmn@users.noreply.github.com>
2025-04-22 20:15:29 +0800

    Add files via upload

---
+++ anchor.js
@@ -0,0 +1,21 @@
+function anchor(position) {
+    document.getElementById("web-container").scrollTop = 0
+    document.getElementById("web-container").scrollTop = document.getElementById("anchor".concat(position.toString())).getBoundingClientRect().y
+}
+
+function loaded() {
+    setTimeout(function() {
+        document.getElementById("body").scrollTop = 0
+    }, 10)
+
+    setTimeout(function() {
+        let url = window.location.href
+    
+        if (url.search("#") != -1) {
+            let id = url.split("#")[1]
+
+            document.getElementById("web-container").scrollTop = 0
+            document.getElementById("web-container").scrollTop = document.getElementById(id).getBoundingClientRect().y
+        }
+    }, 11)
+}
